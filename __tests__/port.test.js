7dfdc13d5839f72d73bfcd6d676c5dd6a25f24de
Ed Crowther <edward930@hotmail.co.uk>
2023-01-09 19:47:54 +0000

    ports have ships part way through, working so far

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -12,4 +12,25 @@ describe('Port', () => {
 
         expect(port.name).toEqual('Dover');
     });
+
+    it('can add a ship', () => {
+        const ship = {};
+        const port = new Port('Dover');
+
+        port.addShip(ship);
+
+        expect(port.ships).toContain(ship);
+    });
+
+    it('can remove a ship', () => {
+        const port = new Port('Dover');
+        const titanic = {};
+        const queenMary = {};
+        
+        port.addShip(titanic);
+        port.addShip(queenMary);
+        port.removeShip(queenMary);
+
+        expect(port.ships).toEqual([titanic]);
+    });
 });
--- src/port.js
+++ src/port.js
@@ -1,5 +1,15 @@
 function Port (name) {
     this.name = name;
-}
+    this.ships = [];
+};
+
+Port.prototype.addShip=function(ship) {
+    this.ships = [ship];
+};
+
+Port.prototype.removeShip=function(ship) {
+    const shipToRemoveIndex = this.ships.indexOf(ship);
+    this.ships = this.ships.splice(shipToRemoveIndex,1);
+};
 
 module.exports = Port

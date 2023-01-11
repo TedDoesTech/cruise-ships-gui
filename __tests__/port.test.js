dfce8eff743a653721cc3a088afafc834acca973
Ed Crowther <edward930@hotmail.co.uk>
2023-01-11 20:26:05 +0000

    port and ship test files DRY'd

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -5,42 +5,39 @@ const Itinerary = require('../src/itinerary.js');
 const Ship = require('../src/ship.js');
 
 describe('Port', () => {
-    it('can be instantiated', () => {
-        expect(new Port()).toBeInstanceOf(Object);
+    describe('it has a name and can add and remove a ship', () => {
+        let port;
+        let ship;
+        let titanic;
+        let queenMary;
+
+    beforeEach(() => {
+        port = new Port('Dover');
+        ship = {};
+        titanic = {};
+        queenMary = {};
+        });
+
+        it('can be instantiated', () => {
+            expect(new Port()).toBeInstanceOf(Object);
+        });
+
+        it('has a name', () => {
+            expect(port.name).toEqual('Dover');
+        });
+
+        it('can add a ship', () => {
+            port.addShip(ship);
+
+            expect(port.ships).toContain(ship);
+        });
+
+        it('can remove a ship', () => {
+            port.addShip(titanic);
+            port.addShip(queenMary);
+            port.removeShip(queenMary);
+
+            expect(port.ships).toEqual([titanic]);
+        });
     });
-
-    it('has a name', () => {
-        const port = new Port('Dover');
-
-        expect(port.name).toEqual('Dover');
-    });
-
-    it('can add a ship', () => {
-        const ship = {};
-        const port = new Port('Dover');
-
-        port.addShip(ship);
-
-        expect(port.ships).toContain(ship);
-    });
-
-    it('can remove a ship', () => {
-        const port = new Port('Dover');
-        const titanic = {};
-        const queenMary = {};
-        
-        port.addShip(titanic);
-        port.addShip(queenMary);
-        port.removeShip(queenMary);
-
-        expect(port.ships).toEqual([titanic]);
-    });
-
-    it('gets added to port on instantiation', () => {
-        const dover = new Port('Dover');
-        const itinerary = new Itinerary([dover]);
-        const ship = new Ship(itinerary);
-      
-        expect(dover.ships).toContain(ship);
-      });
 });

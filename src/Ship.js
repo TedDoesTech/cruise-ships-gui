045856f362e0636267f3cefc0337518d83453e4d
Ed Crowther <edward930@hotmail.co.uk>
2023-01-24 14:25:30 +0000

    part way through adding ports in GUI

--- src/Ship.js
+++ src/Ship.js
@@ -1,30 +1,38 @@
-function Ship (itinerary) {
-    this.itinerary = itinerary
-    this.currentPort = itinerary.ports[0];
-    this.previousPort = null;
-    this.currentPort.addShip(this);
-}
-
-Ship.prototype.setSail=function() {
-    const itinerary = this.itinerary;
-    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
-
-    this.currentPort.removeShip(this);
-
-    if(currentPortIndex === (itinerary.ports.length -1)) {
-        throw new Error('End of itinerary reached');
+(function exportShip() {
+    function Ship (itinerary) {
+        this.itinerary = itinerary
+        this.currentPort = itinerary.ports[0];
+        this.previousPort = null;
+        this.currentPort.addShip(this);
     }
 
-    this.previousPort = this.currentPort
-    this.currentPort=null;
-}
+    Ship.prototype.setSail=function() {
+        const itinerary = this.itinerary;
+        const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
 
-Ship.prototype.dock=function() {
-    const itinerary = this.itinerary;
-    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+        this.currentPort.removeShip(this);
 
-    this.currentPort = itinerary.ports[previousPortIndex +1];
-    this.currentPort.addShip(this);
-}
+        if(currentPortIndex === (itinerary.ports.length -1)) {
+            throw new Error('End of itinerary reached');
+        }
+
+        this.previousPort = this.currentPort
+        this.currentPort=null;
+    }
+
+    Ship.prototype.dock=function() {
+        const itinerary = this.itinerary;
+        const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+        this.currentPort = itinerary.ports[previousPortIndex +1];
+        this.currentPort.addShip(this);
+    }
+
+    if(typeof module !== 'undefined' && module.exports) {
+        module.exports = Ship;
+    } else {
+        window.Ship = Ship;
+    };
+}());
 
 module.exports = Ship
--- src/port.js
+++ src/port.js
@@ -1,17 +1,25 @@
-function Port (name) {
-    this.name = name;
-    this.ships = [];
-};
+(function exportPort() {
+    function Port (name) {
+        this.name = name;
+        this.ships = [];
+    };
 
-Port.prototype.addShip=function(ship) {
-    this.ships.push(ship);
-    return this.ships;
-};
+    Port.prototype.addShip=function(ship) {
+        this.ships.push(ship);
+        return this.ships;
+    };
 
-Port.prototype.removeShip=function(ship) {
-    const shipToRemoveIndex = this.ships.indexOf(ship);
-    this.ships.splice(shipToRemoveIndex,1);
-    return this.ships;
-};
+    Port.prototype.removeShip=function(ship) {
+        const shipToRemoveIndex = this.ships.indexOf(ship);
+        this.ships.splice(shipToRemoveIndex,1);
+        return this.ships;
+    };
+
+    if(typeof module !== 'undefined' && module.exports) {
+        module.exports = Port;
+    } else {
+        window.Port = Port;
+    };
+}());
 
 module.exports = Port
